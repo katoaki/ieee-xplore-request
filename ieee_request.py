566ffc55ed372940018c58916a75bd281e18da33
kato <katoaki@users.noreply.github.com>
2016-07-30 20:33:43 +0900

    Add files via upload

--- ieee_request.py
+++ ieee_request.py
@@ -2,18 +2,53 @@
 
 # first import the requests module to be able to send requests
 import requests
+payload = {}
 
-# asking the user of the program to choose the search type
-print('What search type do you want to use? Enter "querytext" if you want to search for keywords in documents:')
-searchtype1 = input()
-
-# ask the user for the keywords to the search type
-print('Now type the keywords you want to search for in the database:')
+# asking the user for author
+searchtype1 = 'au'
+print('Do you want to search for one or more authors? Type in the names, comma separated, without space. If not just enter')
 keywords1 = input()
 
-# try to assemble the request url a little more automatically by passing parameters with params
+# filling the dictionary payload with searchtype and key, if it was input
+if keywords1:
+    key1 = keywords1.split(',')
+    payload[searchtype1] = key1
 
-payload = {searchtype1: keywords1}
+# search for keywords in title
+searchtype2 = 'ti'
+print('Do you want to search for keywords in the title? Input now or press Enter:')
+keywords2 = input()
+if keywords2:
+    key2 = keywords2.split(',')
+    payload[searchtype2] = key2
+
+# search for keywords in everything: title, abstract, document
+searchtype3 = 'querytext'
+print('If you want to search for keywords in the title, abstract and document, enter the keywords here. IEEE xplore allows a maximum of 10 keywords.')
+keywords3 = input()
+if keywords3:
+    key3 = keywords3.split(',')
+    payload[searchtype3] = key3
+
+# start year to search in publications
+searchtype4 = 'pys'
+print('Year to start the search from:')
+key4 = input()
+if key4:
+    payload[searchtype4] = key4
+
+
+# end year to search in publications
+searchtype5 = 'pye'
+print('Year until to search for publications:')
+key5 = input()
+if key5:
+    payload[searchtype5] = key5
+
+# test to see how payload looks like
+print(payload)
+
+# try to assemble the request url a little more automatically by passing parameters with params
 r = requests.get('http://ieeexplore.ieee.org/gateway/ipsSearch.jsp?', params=payload)
 
 # test if the url is assembled right
