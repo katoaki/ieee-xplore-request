c2f17ab57c9f51c0ecf5a269ca4954702efc167f
kato <katoaki@users.noreply.github.com>
2016-09-07 16:15:10 +0900

    Add files via upload

--- ieee_request.py
+++ ieee_request.py
@@ -8,56 +8,56 @@ import xml.etree.ElementTree as ET
 payload = {}
 
 # asking the user for author
-searchtype1 = 'au'
+key1 = 'au'
 print('Do you want to search for one or more authors? \n'\
       'Type in the names, comma separated, without space. \n'\
       'If not just enter')
 keywords1 = input()
 
-# filling the dictionary payload with searchtype and key, if it was input
+# filling the dictionary payload with key and value, if it was input
 if keywords1:
-    key1 = keywords1.split(',')
-    payload[searchtype1] = key1
+    value1 = keywords1.split(',')
+    payload[key1] = value1
 
 # search for keywords in title
-searchtype2 = 'ti'
+key2 = 'ti'
 print('Do you want to search for keywords in the title? \n'\
       'Input now or press Enter:')
 keywords2 = input()
 if keywords2:
-    key2 = keywords2.split(',')
-    payload[searchtype2] = key2
+    value2 = keywords2.split(',')
+    payload[key2] = value2
 
 # search for keywords in everything: title, abstract, document
-searchtype3 = 'querytext'
+key3 = 'querytext'
 print('If you want to search for keywords in the title, abstract and document, \n'\
       'enter the keywords here. IEEE xplore allows a maximum of 10 keywords.')
 keywords3 = input()
 if keywords3:
-    key3 = keywords3.split(',')
-    payload[searchtype3] = key3
+    value3 = keywords3.split(',')
+    payload[key3] = value3
 
 # start year to search in publications
-searchtype4 = 'pys'
+key4 = 'pys'
 print('Year to start the search from:')
-key4 = input()
-if key4:
-    payload[searchtype4] = key4
+value4 = input()
+if value4:
+    payload[key4] = value4
 
 
 # end year to search in publications
-searchtype5 = 'pye'
+key5 = 'pye'
 print('Year until to search for publications:')
-key5 = input()
-if key5:
-    payload[searchtype5] = key5
+value5 = input()
+if value5:
+    payload[key5] = value5
 
-searchtype6 = 'rs'
-key6 = 1
-payload[searchtype6] = key6
-searchtype7 = 'hc'
-key7 = 1000
-payload[searchtype7] = key7
+key6 = 'rs'
+value6 = 1
+payload[key6] = value6
+key7 = 'hc'
+value7 = 1000
+payload[key7] = value7
 
 # test to see how payload looks like
 print(payload)
@@ -89,8 +89,8 @@ rs = 1
 while total > 1000:
 #    print('rs = ', rs)
     rs = rs + 1000
-    key6 = rs
-    payload[searchtype6] = key6
+    value6 = rs
+    payload[key6] = value6
 #    print(payload)
     r = requests.get('http://ieeexplore.ieee.org/gateway/ipsSearch.jsp?',
 	params=payload)
@@ -106,7 +106,7 @@ for arnumber in z_root.findall('.//arnumber'):
     articleArray.append(arnumber.text)
 
 # access the html website to each articlenumber and in the future search for additional information
-#htmlsuche = requests.get('http://http://ieeexplore.ieee.org/xpls/icp.jsp?',params=payload)
+#htmlsuche = requests.get('http://ieeexplore.ieee.org/xpls/icp.jsp?',params=payload)
 
 #write data into an output file
 #tree = ET.ElementTree(z_root)
